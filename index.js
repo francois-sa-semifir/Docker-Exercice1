548e8ad77c4dc2ff37df65505c273cf5aeab0e56
Francois Salmon <francois.salmon@semifir.com>
2024-04-10 12:55:51 +0200

    fix: :coffin: suppression de code mort

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// let express = require('express');
-// let util = require('util');
-// let app = express();
-// app.get('/', function(req, res){
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end(util.format('%s - %s\n', new Date, 'Request received'));
-// });
-// app.listen(3333)
-
 let express = require('express');
 let util = require('util');
 let app = express();
