c8b72883fb10452a8d48717faaa0d7014501edba
Francois Salmon <francois.salmon@semifir.com>
2024-04-10 12:04:14 +0200

    feat: MAJ .gitignore et Update index.js
    Changement du mesage d'affichage final et ajout d'une fonction de formatage de la date et de l'heure

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+// let express = require('express');
+// let util = require('util');
+// let app = express();
+// app.get('/', function(req, res){
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end(util.format('%s - %s\n', new Date, 'Request received'));
+// });
+// app.listen(3333)
+
 let express = require('express');
 let util = require('util');
 let app = express();
+
 app.get('/', function(req, res){
     res.setHeader('Content-Type', 'text/plain');
-    res.end(util.format('%s - %s\n', new Date, 'Requête reçue'));
+    // Obtenir la date actuelle
+    let currentDate = new Date();
+    // Formater la date et l'heure en français
+    let dateFormatted = currentDate.toLocaleDateString('fr-FR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    let timeFormatted = currentDate.toLocaleTimeString('fr-FR', {hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'Europe/Paris' });
+    // Combinaison de la date et de l'heure
+    let dateTimeFormatted = 'Nous sommes le : ' + dateFormatted + ' - ' + 'il est : ' + timeFormatted;
+    res.end(util.format('%s - %s\n', dateTimeFormatted, 'Bravo votre conteneur Express fonctionne'));
 });
-app.listen(3333)
+
+app.listen(3333);
